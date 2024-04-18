81bfec7382b985d7f4fe9383f23ee27b96f4f691
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-18 22:33:29 +0300

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -5,11 +5,11 @@ from config import SUPPORT_CHAT
 
 keyboard = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton(text="ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ", callback_data="gensession")],
+        [InlineKeyboardButton(text="‹ بدء استخراج الجلسة ›", callback_data="gensession")],
         [
-            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
+            InlineKeyboardButton(text="‹ قـناة الـبوت ›", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://github.com/AnonymousX1025/StringGenBot"
+                text="‹ الـمطور ›", url="https://t.me/ah_2_v"
             ),
         ],
     ]
@@ -18,13 +18,12 @@ keyboard = InlineKeyboardMarkup(
 gen_key = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ v1", callback_data="pyrogram1"),
-            InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ v2", callback_data="pyrogram"),
+            InlineKeyboardButton(text="‹ بايروجرام ›", callback_data="pyrogram"),
+            InlineKeyboardButton(text="‹ تيرمكس ›", callback_data="telethon"),
         ],
-        [InlineKeyboardButton(text="ᴛᴇʟᴇᴛʜᴏɴ", callback_data="telethon")],
     ]
 )
 
 retry_key = InlineKeyboardMarkup(
-    [[InlineKeyboardButton(text="ᴛʀʏ ᴀɢᴀɪɴ", callback_data="gensession")]]
+    [[InlineKeyboardButton(text="‹ بدء استخراج الجلسة ›", callback_data="gensession")]]
 )
