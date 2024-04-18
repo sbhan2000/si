b29346f0e171ec1c3f77e6870020852bf4ec17ef
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-19 00:42:46 +0300

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -8,7 +8,7 @@ from StringGen.utils import add_served_user, keyboard
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"🏮 مرحبا {message.from_user.first_name},\n\n🏮 انا اسمي {Anony.mention},\n🏮 استطيع استخراج جلسات التيرمكس والبايروجرام بسهوله وسرعه وامان.**",
+        text=f"<b>🏮 مرحبا {message.from_user.first_name},\n\n🏮 انا اسمي {Anony.mention},\n🏮 استطيع استخراج جلسات التيرمكس والبايروجرام بسهوله وسرعه وامان.</b>",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
